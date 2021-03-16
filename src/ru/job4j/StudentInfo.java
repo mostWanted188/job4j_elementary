03427abcc458fce9a9cc32f414801f003a0f5e7e
Arslan Akhmetzyanov <Arslan.akhmetzyanov@gmail.com>
2021-03-16 17:14:21 +0300

    3.3. Разбор первой программы. [#499261]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Arslan Akhmetzyanov");
+        System.out.println("04.01.1986");
+    }
+}
