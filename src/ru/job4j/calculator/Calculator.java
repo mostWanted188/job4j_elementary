2a067d500e4548d6a6be9b19958e8e9abe6a1ca7
Arslan Akhmetzyanov <Arslan.akhmetzyanov@gmail.com>
2021-03-17 19:20:32 +0300

    3. Арифметические операции. [#499254]

---
+++ src/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+
+        int result = one + two;
+
+    }
+}
